75af528e047b4eee4cb5ca22eff8d4c310165cf8
rg3915 <regis42santos@gmail.com>
2023-04-09 03:58:54 -0300

    bugfix: axios config

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -4,5 +4,3 @@ import Vue from 'vue'
 axios.defaults.baseURL = 'http://localhost:8000'
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 axios.defaults.headers.common['Accept'] = 'application/json'
-
-Vue.prototype.$axios = axios
