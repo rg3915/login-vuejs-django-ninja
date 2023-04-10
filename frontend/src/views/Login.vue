e3bee80ea1d6ed1a9335c9f0eddf5c9cb3a9e678
rg3915 <regis42santos@gmail.com>
2023-04-10 04:11:12 -0300

    fix typo

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -42,7 +42,7 @@
               class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
               href="../index.html"
             >
-              Log in
+              Login
             </a>
 
             <hr class="my-8" />
