0a040a49ecf867572d35ce846c455eafc2a559c5
rg3915 <regis42santos@gmail.com>
2023-04-21 22:12:55 -0300

    fix: refatora o jeito de esconder o Sidebar e Navigation na tela de login.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex h-screen bg-gray-50 dark:bg-gray-900" id="app">
-    <Sidebar :isSideMenuOpen="isSideMenuOpen" @close-side-menu="isSideMenuOpen = false" />
+    <Sidebar :isSideMenuOpen="isSideMenuOpen" @close-side-menu="isSideMenuOpen = false" v-show="showSidebar" />
 
     <div class="flex flex-col flex-1 w-full">
-      <Navigation @open-side-menu="isSideMenuOpen = !isSideMenuOpen" />
+      <Navigation @open-side-menu="isSideMenuOpen = !isSideMenuOpen" v-show="showNavigation" />
       <div class="p-6 dark:text-white">
         <RouterView />
       </div>
@@ -12,12 +12,17 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue"
 import "@/assets/main.min.css"
 
 import { RouterView } from 'vue-router'
 import Sidebar from './components/Sidebar.vue'
 import Navigation from './components/Navigation.vue'
 
-const isSideMenuOpen = ref(false);
+import {useRoute} from 'vue-router'
+const route = useRoute()
+const currentRoute = computed(() => route.path)
+
+const showSidebar = computed(() => currentRoute.value !== '/login')
+const showNavigation = computed(() => currentRoute.value !== '/login')
 </script>
